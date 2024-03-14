41e93bf377b59ed27b2ec1129693b9984e128a7d
akhilvarma08 <adprince15@.com>
2024-03-14 11:08:00 +0000

    added file

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+HUMAN1=kush
+HUMAN2=pradeep
+
+echo "$HUMAN1:hey how you doing?"
+echo "$HUMAN2:im good thanks how are you?"
+echo "$HUMAN1:im good as well,hows life going?"
+echo "$HUMAN2:all well thanks"
